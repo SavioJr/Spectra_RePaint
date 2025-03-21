c5b34291964a3c5325a035c5b43cf372d8ec6aa1
Domingos Savio <domingos.pinheirodonascimentoj@inaf.it>
2025-03-21 23:14:22 +0100

    Added function and class for loading .npy files; Removed PIL package

--- guided_diffusion/image_datasets.py
+++ guided_diffusion/image_datasets.py
@@ -1,30 +1,15 @@
-# Copyright (c) 2022 Huawei Technologies Co., Ltd.
-# Licensed under CC BY-NC-SA 4.0 (Attribution-NonCommercial-ShareAlike 4.0 International) (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
-#
-# The code is released for academic research use only. For commercial use, please contact Huawei Technologies Co., Ltd.
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This repository was forked from https://github.com/openai/guided-diffusion, which is under the MIT license
-
 import random
 import os
 
-from PIL import Image
 import blobfile as bf
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
 
+
 def load_data_yield(loader):
     while True:
         yield from loader
+
 
 def load_data_inpa(
     *,
@@ -42,63 +27,52 @@ def load_data_inpa(
     drop_last=True,
     conf=None,
     offset=0,
-    ** kwargs
+    **kwargs
 ):
     """
-    For a dataset, create a generator over (images, kwargs) pairs.
-
-    Each images is an NCHW float tensor, and the kwargs dict contains zero or
-    more keys, each of which map to a batched Tensor of their own.
-    The kwargs dict can be used for class labels, in which case the key is "y"
-    and the values are integer tensors of class labels.
-
-    :param data_dir: a dataset directory.
-    :param batch_size: the batch size of each returned pair.
-    :param image_size: the size to which images are resized.
-    :param class_cond: if True, include a "y" key in returned dicts for class
-                       label. If classes are not available and this is true, an
-                       exception will be raised.
-    :param deterministic: if True, yield results in a deterministic order.
-    :param random_crop: if True, randomly crop the images for augmentation.
-    :param random_flip: if True, randomly flip the images for augmentation.
+    Load spectral patches from either `.npy` files or image directories.
     """
 
-    gt_dir = os.path.expanduser(gt_path)
-    mask_dir = os.path.expanduser(mask_path)
+    if gt_path.endswith(".npy") and mask_path.endswith(".npy"):
+        # ✅ If both gt and mask are .npy files, load them as NumPy datasets
+        gt_data = np.load(gt_path)  # Ground truth
+        mask_data = np.load(mask_path)  # Corresponding masks
 
-    gt_paths = _list_image_files_recursively(gt_dir)
-    mask_paths = _list_image_files_recursively(mask_dir)
-
-    assert len(gt_paths) == len(mask_paths)
-
-    classes = None
-    if class_cond:
-        raise NotImplementedError()
-
-    dataset = ImageDatasetInpa(
-        image_size,
-        gt_paths=gt_paths,
-        mask_paths=mask_paths,
-        classes=classes,
-        shard=0,
-        num_shards=1,
-        random_crop=random_crop,
-        random_flip=random_flip,
-        return_dict=return_dict,
-        max_len=max_len,
-        conf=conf,
-        offset=offset
-    )
-
-    if deterministic:
-        loader = DataLoader(
-            dataset, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=drop_last
+        dataset = NpyDatasetInpa(
+            gt_data,
+            mask_data,
+            image_size=image_size,
+            random_crop=random_crop,
+            random_flip=random_flip,
+            return_dict=return_dict,
+            max_len=max_len,
         )
 
     else:
-        loader = DataLoader(
-            dataset, batch_size=batch_size, shuffle=True, num_workers=1, drop_last=drop_last
+        # ✅ Default behavior for loading images from directories
+        gt_dir = os.path.expanduser(gt_path)
+        mask_dir = os.path.expanduser(mask_path)
+
+        gt_paths = _list_image_files_recursively(gt_dir)
+        mask_paths = _list_image_files_recursively(mask_dir)
+
+        assert len(gt_paths) == len(mask_paths)
+
+        dataset = ImageDatasetInpa(
+            image_size,
+            gt_paths=gt_paths,
+            mask_paths=mask_paths,
+            random_crop=random_crop,
+            random_flip=random_flip,
+            return_dict=return_dict,
+            max_len=max_len,
+            conf=conf,
+            offset=offset,
         )
+
+    loader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=not deterministic, num_workers=1, drop_last=drop_last
+    )
 
     if return_dataloader:
         return loader
@@ -107,32 +81,86 @@ def load_data_inpa(
 
 
 def _list_image_files_recursively(data_dir):
+    """
+    Recursively list all image files in a directory.
+    """
     results = []
     for entry in sorted(bf.listdir(data_dir)):
         full_path = bf.join(data_dir, entry)
-        ext = entry.split(".")[-1]
-        if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "gif"]:
+        ext = entry.split(".")[-1].lower()
+        if ext in ["jpg", "jpeg", "png", "gif"]:
             results.append(full_path)
         elif bf.isdir(full_path):
             results.extend(_list_image_files_recursively(full_path))
     return results
 
 
+class NpyDatasetInpa(Dataset):
+    """
+    Dataset class for handling .npy spectral patches for RePaint.
+    """
+
+    def __init__(self, gt_data, mask_data, image_size, random_crop=False, random_flip=True, return_dict=False, max_len=None):
+        super().__init__()
+        self.gt_data = gt_data  # Loaded spectral patches
+        self.mask_data = mask_data  # Corresponding masks
+        self.image_size = image_size
+        self.random_crop = random_crop
+        self.random_flip = random_flip
+        self.return_dict = return_dict
+        self.max_len = max_len
+
+    def __len__(self):
+        return min(len(self.gt_data), self.max_len) if self.max_len else len(self.gt_data)
+
+    def __getitem__(self, idx):
+        arr_gt = self.gt_data[idx]  # Extract ground truth patch
+        arr_mask = self.mask_data[idx]  # Extract corresponding mask
+
+        if self.random_crop:
+            arr_gt = random_crop_arr(arr_gt, self.image_size)
+            arr_mask = random_crop_arr(arr_mask, self.image_size)
+        else:
+            arr_gt = center_crop_arr(arr_gt, self.image_size)
+            arr_mask = center_crop_arr(arr_mask, self.image_size)
+
+        if self.random_flip and random.random() < 0.5:
+            arr_gt = arr_gt[:, ::-1]
+            arr_mask = arr_mask[:, ::-1]
+
+        arr_gt = arr_gt.astype(np.float32)  # Keep as float32
+        arr_mask = arr_mask.astype(np.float32)
+
+        if len(arr_gt.shape) == 2:  # Ensure grayscale compatibility
+            arr_gt = np.expand_dims(arr_gt, axis=0)
+            arr_mask = np.expand_dims(arr_mask, axis=0)
+
+        if self.return_dict:
+            return {
+                'GT': arr_gt,
+                'GT_name': f"patch_{idx:05d}",
+                'gt_keep_mask': arr_mask,
+            }
+        else:
+            return arr_gt, arr_mask
+
+
 class ImageDatasetInpa(Dataset):
+    """
+    Dataset class for handling image file inputs.
+    """
+
     def __init__(
         self,
         resolution,
         gt_paths,
         mask_paths,
-        classes=None,
-        shard=0,
-        num_shards=1,
         random_crop=False,
         random_flip=True,
         return_dict=False,
         max_len=None,
         conf=None,
-        offset=0
+        offset=0,
     ):
         super().__init__()
         self.resolution = resolution
@@ -140,79 +168,70 @@ class ImageDatasetInpa(Dataset):
         gt_paths = sorted(gt_paths)[offset:]
         mask_paths = sorted(mask_paths)[offset:]
 
-        self.local_gts = gt_paths[shard:][::num_shards]
-        self.local_masks = mask_paths[shard:][::num_shards]
-
-        self.local_classes = None if classes is None else classes[shard:][::num_shards]
+        self.local_gts = gt_paths[:max_len] if max_len else gt_paths
+        self.local_masks = mask_paths[:max_len] if max_len else mask_paths
 
         self.random_crop = random_crop
         self.random_flip = random_flip
         self.return_dict = return_dict
-        self.max_len = max_len
 
     def __len__(self):
-        if self.max_len is not None:
-            return self.max_len
-
         return len(self.local_gts)
 
     def __getitem__(self, idx):
         gt_path = self.local_gts[idx]
-        pil_gt = self.imread(gt_path)
-
         mask_path = self.local_masks[idx]
-        pil_mask = self.imread(mask_path)
+
+        with bf.BlobFile(gt_path, "rb") as f:
+            arr_gt = np.load(f)  # ✅ Load .npy instead of images
+
+        with bf.BlobFile(mask_path, "rb") as f:
+            arr_mask = np.load(f)  # ✅ Load .npy masks
 
         if self.random_crop:
-            raise NotImplementedError()
+            arr_gt = random_crop_arr(arr_gt, self.resolution)
+            arr_mask = random_crop_arr(arr_mask, self.resolution)
         else:
-            arr_gt = center_crop_arr(pil_gt, self.resolution)
-            arr_mask = center_crop_arr(pil_mask, self.resolution)
+            arr_gt = center_crop_arr(arr_gt, self.resolution)
+            arr_mask = center_crop_arr(arr_mask, self.resolution)
 
         if self.random_flip and random.random() < 0.5:
             arr_gt = arr_gt[:, ::-1]
             arr_mask = arr_mask[:, ::-1]
 
-        arr_gt = arr_gt.astype(np.float32) / 127.5 - 1
-        arr_mask = arr_mask.astype(np.float32) / 255.0
+        arr_gt = arr_gt.astype(np.float32)
+        arr_mask = arr_mask.astype(np.float32)
 
-        out_dict = {}
-        if self.local_classes is not None:
-            out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
+        if len(arr_gt.shape) == 2:  # Ensure grayscale format
+            arr_gt = np.expand_dims(arr_gt, axis=0)
+            arr_mask = np.expand_dims(arr_mask, axis=0)
 
         if self.return_dict:
-            name = os.path.basename(gt_path)
             return {
-                'GT': np.transpose(arr_gt, [2, 0, 1]),
-                'GT_name': name,
-                'gt_keep_mask': np.transpose(arr_mask, [2, 0, 1]),
+                'GT': arr_gt,
+                'GT_name': os.path.basename(gt_path),
+                'gt_keep_mask': arr_mask,
             }
         else:
-            raise NotImplementedError()
-
-    def imread(self, path):
-        with bf.BlobFile(path, "rb") as f:
-            pil_image = Image.open(f)
-            pil_image.load()
-        pil_image = pil_image.convert("RGB")
-        return pil_image
+            return arr_gt, arr_mask
 
 
-def center_crop_arr(pil_image, image_size):
-    # We are not on a new enough PIL to support the `reducing_gap`
-    # argument, which uses BOX downsampling at powers of two first.
-    # Thus, we do it by hand to improve downsample quality.
-    while min(*pil_image.size) >= 2 * image_size:
-        pil_image = pil_image.resize(
-            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
-        )
+def center_crop_arr(arr, image_size):
+    """
+    Center crops a NumPy array to the specified image size.
+    """
+    h, w = arr.shape
+    crop_y = (h - image_size) // 2
+    crop_x = (w - image_size) // 2
+    return arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size]
 
-    scale = image_size / min(*pil_image.size)
-    pil_image = pil_image.resize(
-        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
-    )
 
-    arr = np.array(pil_image)
-    crop_y = (arr.shape[0] - image_size) // 2
-    crop_x = (arr.shape[1] - image_size) // 2
-    return arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size]
+def random_crop_arr(arr, image_size, min_crop_frac=0.8, max_crop_frac=1.0):
+    """
+    Randomly crops a NumPy array to the specified image size.
+    """
+    h, w = arr.shape
+    crop_size = random.randint(int(image_size / max_crop_frac), int(image_size / min_crop_frac))
+    start_y = random.randint(0, h - crop_size)
+    start_x = random.randint(0, w - crop_size)
+    return arr[start_y : start_y + crop_size, start_x : start_x + crop_size]
